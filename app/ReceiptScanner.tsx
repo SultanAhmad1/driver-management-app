e902431dab6ab6dc34c7044488e599a62874971a
SultanAhmad1 <sultan@mydigitalpeople.com>
2026-03-16 14:46:18 +0000

    Take snap modal overflow set - U

--- app/ReceiptScanner.tsx
+++ app/ReceiptScanner.tsx
@@ -213,7 +213,7 @@ export default function ReceiptScanner() {
 
         {
           optionSelected === 1 &&
-          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 overflow-h" style={{minHeight: "20vh"}}>
             <div className="bg-white rounded-lg shadow-lg w-full max-w-md mx-4">
               
               {/* Modal Header */}
